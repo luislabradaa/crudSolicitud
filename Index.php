98cd49e73768b1a14c6841790ebe60c042c2ec1b
luislabradaa <87114168+luislabradaa@users.noreply.github.com>
2022-11-11 14:43:06 -0600

    base

---
+++ Index.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Index</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <br>
+        <center>
+            <H1>Registro universidad</H1>
+        </center>
+        <br>
+        <div class="container">
+
+        <form>
+            
+            <div class="mb-3">
+                <label  class="form-label">Nombre</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label  class="form-label">Apellido paterno</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label  class="form-label">Apellido materno</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label  class="form-label">Télefono</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Correo</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label  class="form-label">Télefono de casa</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label  class="form-label">Télefono del padre o madre</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label  class="form-label">Nombre de la madre</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label  class="form-label">Apellido paterno</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label  class="form-label">Apellido materno</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label  class="form-label">Nombre del padre</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label  class="form-label">Apellido paterno</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label  class="form-label">Apellido materno</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label  class="form-label">Bachillerato de procedencia</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label  class="form-label">Especialidad cursada</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label  class="form-label">Promedio obtenido</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="mb-3 form-check">
+                <label class="form-label">Foto del estudiante</label>
+                <input type="file" class="form-control">
+            </div>
+            <div class="mb-3 form-check">
+                <label class="form-label">Certificado</label>
+                <input type="file" class="form-control">
+            </div>
+            <div class="mb-3 form-check">
+                <label class="form-label">Comprobante de domicilio</label>
+                <input type="file" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar solicitud</button>
+        </form>
+           <br>
+        </div>
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ Settings/Connection.php
@@ -0,0 +1,8 @@
+<?php
+
+    $server ="localhost";
+    $user = "luisl";
+    $pass = "1610";
+    $datab="universidad";
+
+    $conexion = mysqli_connect($server, $user, $pass, $datab );
